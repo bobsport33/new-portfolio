cd1dbd116049971a0452bc611952cbb55945bdc8
bobsport33 <77863223+bobsport33@users.noreply.github.com>
2023-07-06 16:21:38 -0500

    mobile card sizing

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -8,109 +8,104 @@ import { colors, media } from "../styles/variables";
 gsap.registerPlugin(ScrollTrigger);
 
 const ProjectCont = styled.section`
+  display: flex;
+  flex-direction: column;
+  height: 4500px;
+
+  @media ${media.tablet} {
+    height: 3800px;
+  }
+
+  @media ${media.mobile} {
+    height: 3600px;
+  }
+
+  .project__title {
+    color: ${colors.neutral100};
+    text-align: center;
+    margin-bottom: 50px;
+  }
+
+  .project__container {
+    height: 80vh;
+    /* height: 100%; */
+    width: fit-content;
     display: flex;
     flex-direction: column;
-    height: 4500px;
+    justify-content: center;
+    align-items: center;
+  }
+  .project__row {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    gap: 100px;
+    margin: 0 100px;
 
     @media ${media.tablet} {
-        height: 3800px;
+      gap: 50px;
+      margin: 0 50px;
+      align-items: flex-start;
     }
-
-    @media ${media.mobile} {
-        height: 3100px;
-    }
-
-    .project__title {
-        color: ${colors.neutral100};
-        text-align: center;
-        margin-bottom: 50px;
-    }
-
-    .project__container {
-        height: 80vh;
-        /* height: 100%; */
-        width: fit-content;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-    }
-    .project__row {
-        display: flex;
-        flex-direction: row;
-        justify-content: space-between;
-        gap: 100px;
-        margin: 0 100px;
-
-        @media ${media.tablet} {
-            gap: 50px;
-            margin: 0 50px;
-            align-items: flex-start;
-        }
-    }
+  }
 `;
 
 interface Project {
-    id: string;
-    title: string;
-    url: string;
-    image: string;
-    alt: string;
+  id: string;
+  title: string;
+  url: string;
+  image: string;
+  alt: string;
 
-    skills: string[];
+  skills: string[];
 }
 
 interface Data {
-    projects: Project[];
+  projects: Project[];
 }
 
 const Projects = ({ projects }: Data) => {
-    const container = useRef(null);
-    gsap.registerPlugin(ScrollTrigger);
+  const container = useRef(null);
+  gsap.registerPlugin(ScrollTrigger);
 
-    useLayoutEffect(() => {
-        const ctx = gsap.context((self: gsap.Context) => {
-            if (!self.selector) {
-                return;
-            }
-            const box = self.selector(".project__container");
-            const row = self.selector(".project__row");
-            let rowWidth = row[0].offsetWidth;
+  useLayoutEffect(() => {
+    const ctx = gsap.context((self: gsap.Context) => {
+      if (!self.selector) {
+        return;
+      }
+      const box = self.selector(".project__container");
+      const row = self.selector(".project__row");
+      let rowWidth = row[0].offsetWidth;
 
-            gsap.to(box, {
-                xPercent: -100,
-                x: () => innerWidth,
-                ease: "none",
-                scrollTrigger: {
-                    trigger: box,
-                    start: "top 10%",
-                    end: `+=${rowWidth}`,
-                    scrub: true,
-                    pin: true,
-                    invalidateOnRefresh: true,
-                },
-            });
-        }, container);
+      gsap.to(box, {
+        xPercent: -100,
+        x: () => innerWidth,
+        ease: "none",
+        scrollTrigger: {
+          trigger: box,
+          start: "top 10%",
+          end: `+=${rowWidth}`,
+          scrub: true,
+          pin: true,
+          invalidateOnRefresh: true,
+        },
+      });
+    }, container);
 
-        return () => {
-            ctx.revert();
-        };
-    }, []);
+    return () => {
+      ctx.revert();
+    };
+  }, []);
 
-    return (
-        <ProjectCont ref={container} id="projects">
-            <h3 className="project__title">Projects</h3>
-            <div className="project__container">
-                <div className="project__row">
-                    {projects.map((project, index) => {
-                        return (
-                            <ProjectCard
-                                data={project}
-                                key={`project-${index}`}
-                            />
-                        );
-                    })}
-                    {/* <ProjectCard
+  return (
+    <ProjectCont ref={container} id="projects">
+      <h3 className="project__title">Projects</h3>
+      <div className="project__container">
+        <div className="project__row">
+          {projects.map((project, index) => {
+            return <ProjectCard data={project} key={`project-${index}`} />;
+          })}
+          {/* <ProjectCard
                         data={{
                             id: "1",
                             title: "this is a title",
@@ -194,10 +189,10 @@ const Projects = ({ projects }: Data) => {
                                 "This is a description based off the image and title above. This will be adjusted for each card.",
                         }}
                     /> */}
-                </div>
-            </div>
-        </ProjectCont>
-    );
+        </div>
+      </div>
+    </ProjectCont>
+  );
 };
 
 export default Projects;
